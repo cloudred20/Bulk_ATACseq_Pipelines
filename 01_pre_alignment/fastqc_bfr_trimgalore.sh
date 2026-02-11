07a600dfc5d2c4e6e974ca27a1c39bf7fefdce9a
Megha Lal <meghalal110@gmail.com>
2026-02-11 16:36:24 -0500

    Update fastqc_bfr_trimgalore.sh with assumptions
    Added assumptions for FastQC script and clarified output directory.

--- 01_pre_alignment/fastqc_bfr_trimgalore.sh
+++ 01_pre_alignment/fastqc_bfr_trimgalore.sh
@@ -2,9 +2,16 @@
 
 # Runs FastQC on all FASTQ files before trimming
 
+# Assumes:
+#  - FASTQ files are in ./fastq_files
+#  - FastQC is installed at ~/tools/FastQC/fastqc (edit if different)
+#  - multiqc is available in the conda environment
+
+# Create output directory
 mkdir -p ./fastqc_results
 
-# Run FastQC
+# Run FastQC on all gzipped FASTQ files
+# Output reports (.html + .zip) will be written to fastqc_results
 ~/tools/FastQC/fastqc ./fastq_files/*.fastq.gz -o ./fastqc_results
 
 # Run MultiQC
