a6a61f69aebc5170ff6d391210aa12947ec55516
Megha Lal <meghalal110@gmail.com>
2026-02-11 16:30:15 -0500

    Refactor bowtie2build.sh for genome index generation
    Updated script to generate genome indexes for hg38 using Bowtie2. Changed file handling for the genome assembly file and added directory creation.

---
+++ 01_pre_alignment/bowtie2build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#script to generate genome indexes for hg38 using Bowtie2
+module load Bowtie2/
+
+mkdir bowtie2
+cd bowtie2 
+
+# ENSEMBL annotation  
+wget https://ftp.ensembl.org/pub/release-110/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+bowtie2-build Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz Homo_sapiens
+
+# UCSC annotation
+wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+bowtie2-build hg38.fa.gz hg38
