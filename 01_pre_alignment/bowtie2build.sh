380581b3b92d6438745318fe92dddba8166c67e4
Megha Lal <meghalal110@gmail.com>
2026-02-12 15:41:58 -0500

    Enhance comments in bowtie2build.sh script
    Updated comments for clarity and added additional context to the script.

--- 01_pre_alignment/bowtie2build.sh
+++ 01_pre_alignment/bowtie2build.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
-# Generate genome indexes for hg38 using Bowtie2
+# Generate Bowtie2 genome indexes for human genome (hg38)
+# This script downloads reference FASTA files and builds Bowtie2 indexes
+# Both ENSEMBL and UCSC versions are included
 
-# Load Bowtie2 module
+# Load Bowtie2 module (required if using a module-based system)
 module load Bowtie2/
 
+# Create output directory and move into it
 mkdir bowtie2
 cd bowtie2 
 
-# ENSEMBL annotation  
+# 1. ENSEMBL annotation
+# Downloads the primary assembly FASTA from ENSEMBL and builds index
 wget https://ftp.ensembl.org/pub/release-110/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+
+# Build Bowtie2 index for ENSEMBL reference
+# Output files will be prefixed with "Homo_sapiens"
 bowtie2-build Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz Homo_sapiens
 
-# UCSC annotation
+# 2. UCSC annotation
+# Downloads hg38 FASTA from UCSC and builds index
 wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+
+# Build Bowtie2 index for UCSC reference
+# Output files will be prefixed with "hg38"
 bowtie2-build hg38.fa.gz hg38
